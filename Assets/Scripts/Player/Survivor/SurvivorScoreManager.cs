3e154bc917537f5bd8ca1d1f245aad6766fd64f7
BastienMedina <medina.bastien13@gmail.com>
2026-03-09 12:24:54 +0100

    creation ui upgrade

---
+++ Assets/Scripts/Player/Survivor/SurvivorScoreManager.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SurvivorScoreManager : MonoBehaviour
+{
+    [SerializeField] private ScoresCount _scoreCount;
+    [SerializeField] private Slider _scoreSlider;
+}
