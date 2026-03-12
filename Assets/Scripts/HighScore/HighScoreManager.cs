6bfce1d79700e54cea4b937e0c5ceb88155c6b56
BastienMedina <medina.bastien13@gmail.com>
2026-03-12 11:21:59 +0100

    systeme de high score fonctionnele et placer

--- Assets/Scripts/HighScore/HighScoreManager.cs
+++ Assets/Scripts/HighScore/HighScoreManager.cs
@@ -6,14 +6,42 @@ public class HighScoreManager : MonoBehaviour
     [System.Serializable]
     private class DataScores
     {
-        [SerializeField] private string gameName;
-        [SerializeField] private ScoresCount _score;
+        public string gameName;
+        public ScoresCount _score;
     }
 
-    [SerializeField] private DataScores _dataScores;
+    [SerializeField] private DataScores[] _dataScores;
     [SerializeField] private HighScores _highScore;
+
+    void Start()
+    {
+        SetAllHigh();
+    }
+
     public void SetAllHigh()
     {
-        
+        foreach (var data in _dataScores)
+        {
+            bool found = false;
+
+            foreach (var entry in _highScore.highScores)
+            {
+                if (entry.gameName == data.gameName)
+                {
+                    entry.highScore = data._score.Score; // ou ta méthode
+                    found = true;
+                    break;
+                }
+            }
+
+            if (!found)
+            {
+                HighScores.HighScoreEntry newEntry = new HighScores.HighScoreEntry();
+                newEntry.gameName = data.gameName;
+                newEntry.highScore = data._score.Score;
+
+                _highScore.highScores.Add(newEntry);
+            }
+        }
     }
 }
--- Assets/Scripts/UI/SetTextHigh.cs
+++ Assets/Scripts/UI/SetTextHigh.cs
@@ -1,7 +1,25 @@
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class SetTextHigh : MonoBehaviour
 {
-    
+    [SerializeField] private HighScores _highScores;
+    [SerializeField] private VerticalLayoutGroup _vertBox;
+    [SerializeField] private GameObject _text;
+
+    void Start()
+    {
+        SetText();
+    }
+
+    void SetText()
+    {
+        foreach(var data in _highScores.highScores)
+        {
+            string _textHigh = $"{data.gameName} : {data.highScore}";
+            GameObject obj = Instantiate(_text, _vertBox.transform);
+            obj.GetComponent<TextMeshProUGUI>().text = _textHigh;
+        }
+    }
 }
